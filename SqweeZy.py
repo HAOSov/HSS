e31086732a337cd7f01c5dbf772675a8eedccbf0
HAOSov <haosovandrej@gmail.com>
2020-03-07 15:34:46 +0200

    SqweeZy

---
+++ SqweeZy.py
@@ -0,0 +1,17 @@
+import random
+Z='Nexia'
+X='Nexia'
+print ('SqweeZy drive in?')
+input('Press ENTER to TRUST!')
+print (random.choice([Z,X]))
+input('ODNAKO)0 pravda')
+A='SqweeZy'
+B='always'
+C='drive'
+D='in'
+E='Nexia'
+#print (C + A + D + E)
+#print (A + 'in' + E)
+print ('Nexia' + D +'SqweeZy')
+input('Dont fuck witch SqweeZy')
+input()
