d3c52e300c231bd406761ba41460577a5e37a86b
HAOSov <haosovandrej@gmail.com>
2020-03-07 16:10:43 +0200

    SqweeZy in RU

---
+++ SqweeZyRU.py
@@ -0,0 +1,18 @@
+import random
+import encodings.utf_8
+Z='НЕКСИЯ'
+X='НЕКСИЯ'
+print ('Какая машина у Сквизи?...')
+input('Нажми ENTER чтоб узнать правду')
+print (random.choice([Z,X]))
+input('Нексия, как неожиданно!')
+A='SqweeZy'
+B='всегда'
+C='ездит'
+D='на'
+E='Nexia'
+#print (C + A + D + E)
+#print (A + 'in' + E)
+#print ('Нексия' + D +'SqweeZy')
+input('Не доебывайся до SqweeZy')
+input()
